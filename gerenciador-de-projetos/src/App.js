4c51410edbe966a7944525fd31a4723174d28cbd
Renan Cesar <cesar.renan@hotmail.com>
2022-06-28 22:05:01 -0300

    Usando Fragments

--- gerenciador-de-projetos/src/App.js
+++ gerenciador-de-projetos/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import './index.css';
 import HelloWorld from './Components/HelloWorld';
 import SayMyName from './Components/SayMyName';
 import Pessoa from './Components/Pessoa';
 import Frase from './Components/Frase';
+import List from './Components/List';
 
 function App() {
   const nome = "Cesar";
@@ -19,6 +21,7 @@ function App() {
           profissao={"Desenvolvedor front-end"}
           foto="https://via.placeholder.com/150"
         />
+        <List />
     </div>
   );
 }
